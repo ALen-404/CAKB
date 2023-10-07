e36f352065ad525f231f764e842d8c207492be98
alen <kangfaliao@gmail.com>
2023-10-07 14:55:57 +0800

    first commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,68 @@
+import { formatAmount, shorten } from '@did-network/dapp-sdk'
+import { useAccount } from 'wagmi'
+
+import { Header } from '@/components/layout/Header'
+import { NetworkSwitcher } from '@/components/SwitchNetworks'
+import { Button } from '@/components/ui/button'
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card'
+import { useToast } from '@/components/ui/use-toast'
+import { WalletModal } from '@/components/WalletModal'
+import { useWagmi } from '@/hooks'
+import { useCopyToClipboard } from '@/hooks/useCopy'
+
+const Home = () => {
+  const { address } = useAccount()
+
+  const [show, setShow] = useState(false)
+
+  const toggleModal = (e: boolean) => {
+    setShow(e)
+  }
+
+  const [_, copy] = useCopyToClipboard()
+  const { toast } = useToast()
+
+  const copyHandler = useCallback(() => {
+    copy('pnpm dlx fisand')
+
+    toast({
+      title: 'Copied success!',
+    })
+  }, [copy, toast])
+
+  return (
+    <>
+      <Header
+        action={
+          <>
+            <NetworkSwitcher />
+            <WalletModal open={show} onOpenChange={toggleModal} close={() => setShow(false)}>
+              {({ isLoading }) => (
+                <Button className="flex items-center mr-4">
+                  {isLoading && (
+                    <span className="i-line-md:loading-twotone-loop inline-flex mr-1 w-4 h-4 text-white"></span>
+                  )}{' '}
+                  {address ? shorten(address) : 'Connect Wallet'}
+                </Button>
+              )}
+            </WalletModal>
+          </>
+        }
+      />
+
+      <p>
+        {['', '', '', ''].map((_, index) => (
+          <Item key={index} />
+        ))}
+      </p>
+    </>
+  )
+}
+
+export default Home
+
+function Item() {
+  const { status } = useWagmi()
+
+  return <span></span>
+}
